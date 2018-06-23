ed7b7aebf14b94518fb2232ecc57afb68cd4ef85
Yaohan Hu <yhhu@CNyhhu.local>
2018-06-24 00:20:12 +0800

    modify

--- frontend/src/pages/Score.js
+++ frontend/src/pages/Score.js
@@ -38,10 +38,10 @@ class Score extends Component {
         <Header className="normal-title">xxx考试系统</Header>
         <Content className="score-content normal-content">
           <div className="score-sheet">
-            <div className="score-sheet-title">考试成绩单</div>
+            <div className="score-sheet-title"><b>Congratulations</b></div>
             {/* <Table columns={ columns } dataSource={ data } pagination={ false } bordered /> */}
             <div className="score-sheet-footer">
-              <div className="score-sheet-footer-label">总分</div>
+              {/* <div className="score-sheet-footer-label">总分</div> */}
               <div className="score-sheet-footer-total">{ this.props.params.num }</div>
             </div>
           </div>
