fcd87ea225d87436cdc13dc12db21e82d8dfacba
Feng Zhang <fezhang@thoughtworks.com>
2018-06-23 20:46:41 +0800

    delete .iml

--- backend/src/main/java/com/thoughtworks/exam/HelloController.java
+++ backend/src/main/java/com/thoughtworks/exam/HelloController.java
@@ -1,4 +1,4 @@
-package hello;
+package com.thoughtworks.exam;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
--- backend/src/test/java/com/thoughtworks/exam/QuestionTest.java
+++ backend/src/test/java/com/thoughtworks/exam/QuestionTest.java
@@ -1,4 +1,33 @@
 package com.thoughtworks.exam;
 
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class QuestionTest {
+
+
+    @Test
+    public void should_return_questions() throws Exception {
+//        mvc.perform(get("/api/queryAll"))
+//                .andExpect(status().isOk())
+//                .andExpect(content().contentTypeCompatibleWith(APPLICATION_JSON_VALUE))
+//                .andExpect(jsonPath("$", Matchers.notNullValue()));
+    }
 }
